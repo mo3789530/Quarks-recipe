632c76e48622544bff2f115b6afe13f793352420
mo3789530 <mo3789530@gmail.com>
2021-02-07 22:15:28 +0900

    add open api

--- hibernate-orm-quickstart/src/main/kotlin/org/acme/rest/json/Fruit.kt
+++ hibernate-orm-quickstart/src/main/kotlin/org/acme/rest/json/Fruit.kt
@@ -1,5 +1,10 @@
+
 package org.acme.rest.json
 
+import javax.persistence.PersistenceUnit
+
+
+import io.quarkus.agroal.DataSource
 import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntity
 import javax.persistence.Cacheable
 import javax.persistence.Column
@@ -7,6 +12,7 @@ import javax.persistence.Entity
 
 @Entity
 @Cacheable
+@PersistenceUnit(name = "test")
 data class Fruit(
     @Column(length = 40, unique = true)
     var name: String = ""
